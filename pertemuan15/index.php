383fb4d3f5faa54601dadc6b097bac2e36accf4d
nabilandak <nocankus256@gmail.com>
2024-02-01 22:12:22 +0700

    Update Terbaru

---
+++ pertemuan15/index.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['login'])){
+    header('Location: Login.php');
+    exit;
+}
+require 'functions.php';
+$game = query("SELECT * FROM game");
+
+// tombol cari ditekan
+    if(isset($_POST["cari"])){
+        $game = cari($_POST["keyword"]);
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <a href="logout.php">Logout</a>
+    <h1>Daftar Game</h1>
+    <a href="tambah-game.php">Tambah Data Game</a>
+
+    <br>
+    <br>
+
+    <form action="" method="post">
+        <input type="text" name="keyword" size="30px" autofocus placeholder="masukan keyword pencarian" autocomplete="off">
+        <button type="submit" name="cari">Cari!</button>
+    </form>
+    <br>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Foto</th>
+            <th>Nama</th>
+            <th>Pengembang</th>
+            <th>Genre</th>
+            <th>Tahun</th>
+            <th>Harga</th>
+        </tr>
+        <?php $i = 1; ?>
+        <?php foreach($game as $g): ?>
+        <tr>
+            <td><?php echo $i; ?></td>
+            <td>
+                <a href="ubah.php?id=<?php echo $g["id"];?>">Edit</a> | 
+                <a href="hapus.php?id=<?php echo $g["id"];?>" onclick="return confirm('Yakin?')">Hapus</a>
+            </td>
+            <td><img src="img/<?php echo $g["foto"]; ?>" alt="" width="50px"></td>
+            <td><?php echo $g["nama"]?></td>
+            <td><?php echo $g["pengembang"]?></td>
+            <td><?php echo $g["genre"]?></td>
+            <td><?php echo $g["tahun"]?></td>
+            <td><?php echo $g["harga"]?></td>
+        </tr>
+        <?php $i++ ?>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
---
+++ pertemuan15/tambah-game.php
@@ -0,0 +1,72 @@
+<?php 
+session_start();
+
+if(!isset($_SESSION['login'])){
+    header('Location: Login.php');
+    exit;
+}
+require 'functions.php';
+
+// cek apakah tombol submit sudah ditekan atau belum
+if(isset($_POST["submit"])){
+// cek apakah data berhasil ditambahkan atau tidak
+ 
+
+    if(tambah($_POST) > 0){
+        echo "<script>
+                alert('Data berhasil ditambahkan!');
+                document.location.href = 'index.php';
+            </script>";
+    }else{
+        echo "<script>
+                alert('Data gagal ditambahkan!');
+                document.location.href = 'index.php';
+            </script>";
+    }
+};
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data Game</title>
+</head>
+<body>
+    <h1>Tambah Data Game</h1>
+    <a href="index.php">Kembali</a>
+    <form action="" method="post" enctype="multipart/form-data">
+        <ul>
+            <li>
+                <label for="nama">Nama : </label>
+                <input type="text" name="nama" id="nama" required>
+            </li>
+            <li>
+                <label for="pengembang">pengembang : </label>
+                <input type="text" name="pengembang" id="pengembang" required>
+            </li>
+            <li>
+                <label for="genre">genre : </label>
+                <input type="text" name="genre" id="genre" required>
+            </li>
+            <li>
+                <label for="tahun">tahun : </label>
+                <input type="text" name="tahun" id="tahun" required>
+            </li>
+            <li>
+                <label for="harga">harga : </label>
+                <input type="text" name="harga" id="harga" required>
+            </li>
+            <li>
+                <label for="foto">foto : </label>
+                <input type="file" name="foto" id="foto" required>
+            </li>
+            <li><button type="submit" name="submit">Tambah Data</button></li>
+        </ul>
+    </form>
+</body>
+</html>
